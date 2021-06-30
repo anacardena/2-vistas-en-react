73f778e4b3695ad865847449a43ca9b86ca4b4a9
AnaCardenas <abcardenasb@gmail.com>
2021-06-30 12:49:51 -0500

    arreglar botones

--- vistas-en-react/src/components/Inicio.jsx
+++ vistas-en-react/src/components/Inicio.jsx
@@ -4,15 +4,37 @@ import {Link} from "react-router-dom"
 
 function Title() {
 	return (
-		<div className="Container_Title">
-		<h1 id="titulop"> ¡bienvenidos!</h1>
-		<Link to="/Nosotras">
-		<button type="button">¿quienes somos?</button>
-		</Link>
-		<Link to="/Ayuda">
-		<button type="button"> Nosotras </button>
-		</Link>
-        </div>
+		<div className="padre">
+		<div className="titulo"> ¡bienvenidos!</div>
+		<div className="container">
+           <Link to="/Nosotras" class="btn"> 
+            <svg width="277" height="62"> 
+                <defs>
+                <linearGradient id="grad1"> 
+                <stop offset="0%" stop-color="#FF8282"/> <stop offset="100%" stop-color="#E178ED" /> 
+                </linearGradient> 
+                </defs> <rect x="5" y="5" rx="25" fill="none" stroke="url(#grad1)" width="266" height="50"></rect> 
+            </svg> 
+            <span>¿quienes somos?</span>
+            </Link> 
+     </div>
+      <div className="container">
+           <Link to="/Ayuda" class="btn"> 
+            <svg width="277" height="62"> 
+                <defs>
+                <linearGradient id="grad1"> 
+                <stop offset="0%" stop-color="#FF8282"/> <stop offset="100%" stop-color="#E178ED" /> 
+                </linearGradient> 
+                </defs> <rect x="5" y="5" rx="25" fill="none" stroke="url(#grad1)" width="266" height="50"></rect> 
+            </svg> 
+            <span>Casas de Ayuda</span>
+            </Link> 
+     </div>
+     </div>
 		)	
+
 };
 export default Title
+
+
+       
