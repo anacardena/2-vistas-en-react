30f9ad385d2c9c2dbbd707ddb114f84f2bf87225
AnaCardenas <abcardenasb@gmail.com>
2021-07-03 21:03:51 -0500

    ya casi termino la página de cómo ayudar

--- vistas-en-react/src/components/Casas_de_ayuda.jsx
+++ vistas-en-react/src/components/Casas_de_ayuda.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
 import '../utils/css/ayuda_estilos.css'
-
+import tres from '../utils/images/parrafo3.jpg'
+import dos from '../utils/images/parrafo2.jpg'
+import uno from '../utils/images/localidad.png'
+import titulo from '../utils/images/titulo.png'
 
 function Casas_de_ayuda () {
     return (
         <div id="Padre">
-       <div id="Titulo_principal"> Casas de igualdad de oportunidades para las mujeres </div>
-
+         <div style={{backgroundImage:`url(${titulo})`}} id="Titulo_principal">
+       Casas de igualdad de oportunidades para las mujeres 
+          </div>
        <div id="Contenido">
 
-      <div className="parrafo1">
+      <div style={{backgroundImage:`url(${uno})`}}>
+               <div class="parrafo1 slideRight">
             <h2> ¿Qué son las casa de ayuda? </h2>
              <p> son espacios de encuentro entre mujeres para el conocimiento de sus derechos y el desarrollo de procesos orientados al empoderamiento social y político. Son el espacio privilegiado para que la  Política Pública de Mujeres y Equidad de Género  llegue con presencia, acompañamiento y servicios a las mujeres diversas que habitan el territorio rural y urbano de Bogotá.</p>
+               </div>
         </div>
 
-        <div className="secciones">
+         <div id="parrafo2"  style={{backgroundImage:`url(${dos})`}}>
+                <div class="parrafo2 slideLeft">
                 <h2> ¿Cuáles son los Objetivos de las Casas de Igualdad de Oportunidades? </h2>
                 <ul>
                 <li> Difundir los derechos de las mujeres y los mecanismos para la exigibilidad de los mismos, con el fin de garantizar el ejercicio pleno de su ciudadanía. </li>
@@ -23,9 +30,11 @@ function Casas_de_ayuda () {
               <li>Promover la participación y representación social y política de las mujeres en las localidades. </li>
                <li> Propiciar en el ámbito local la articulación interinstitucional y transectorial con autoridades administrativas competentes para garantizar el derecho de las mujeres a una vida libre de violencias. </li>
                </ul>
+               </div>
          </div>
 
-          <div className="secciones">
+          <div  id="parrafo3" style={{backgroundImage:`url(${tres})`}}>
+               <div class="parrafo3  slideRight">
               <h2> ¿Qué encuentran las mujeres en las Casas de Igualdad de Oportunidades? </h2>
                 <ul> 
                   <li>Orientación y asesoría socio-jurídica: A través de abogadas especialistas en derechos de las mujeres y enfoque de género, se brinda orientación y asesoría socio jurídica de acuerdo con el tema consultado.</li>
@@ -34,6 +43,7 @@ function Casas_de_ayuda () {
                  <li>Acciones para el empoderamiento de las mujeres: Se promueve la participación así como la representación social y política de las mujeres en las localidades. </li>
                 <li>Centros de Inclusión Digital: Son espacios para reconocer las voces de las mujeres y promover el intercambio de saberes, a través del acceso y uso de tecnologías de información y comunicación (TIC), buscando así disminuir la brecha digital de género.</li>
              </ul>
+             </div>
         </div>
         
 
